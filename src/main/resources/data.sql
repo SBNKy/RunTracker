e9358e86e21ecc318558094be740704f589220da
Sebastian Konieczny <s.konieczny3211@gmail.com>
2025-07-31 19:35:29 +0200

    edit data.sql to insert more data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,9 @@
 INSERT INTO USERS (first_name, last_name, email, password, created_at)
-VALUES ('John', 'Hophins', 'hopkins@gmail.com', 'jhopkins1234', CURRENT_TIMESTAMP);
+VALUES ('John', 'Hopkins', 'john.hopkins@example.com', 'jhopkins1234', CURRENT_TIMESTAMP),
+       ('Emily', 'Johnson', 'emily.johnson@example.com', 'ejohnson5678', CURRENT_TIMESTAMP),
+       ('Michael', 'Smith', 'michael.smith@example.com', 'msmith9012', CURRENT_TIMESTAMP),
+       ('Jessica', 'Williams', 'jessica.williams@example.com', 'jwilliams3456', CURRENT_TIMESTAMP),
+       ('David', 'Brown', 'david.brown@example.com', 'dbrown7890', CURRENT_TIMESTAMP),
+       ('Sarah', 'Jones', 'sarah.jones@example.com', 'sjones2345', CURRENT_TIMESTAMP),
+       ('Daniel', 'Miller', 'daniel.miller@example.com', 'dmiller6789', CURRENT_TIMESTAMP),
+       ('Laura', 'Davis', 'laura.davis@example.com', 'ldavis0123', CURRENT_TIMESTAMP);
