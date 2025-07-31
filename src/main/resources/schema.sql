e7c11b2709f651a979664af4a398c6d720ab7a3a
Sebastian Konieczny <s.konieczny3211@gmail.com>
2025-07-31 19:04:02 +0200

    change DB from H2 to PostgrSQL container

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,23 +4,23 @@ DROP TABLE IF EXISTS users;
 -- Users table
 CREATE TABLE users
 (
-    id         INTEGER AUTO_INCREMENT PRIMARY KEY,
+    id         SERIAL PRIMARY KEY,
     first_name VARCHAR(100),
     last_name  VARCHAR(100),
     email      VARCHAR(255) UNIQUE NOT NULL,
     password   VARCHAR(255)        NOT NULL,
-    created_at TIMESTAMP NOT NULL
+    created_at TIMESTAMP           NOT NULL
 );
 
 -- Runs table
 CREATE TABLE runs
 (
-    id                INTEGER AUTO_INCREMENT PRIMARY KEY,
-    user_id           INTEGER        NOT NULL,
-    name              VARCHAR(255),
-    distance          DECIMAL(10, 2) NOT NULL,
-    started_at        TIMESTAMP      NOT NULL,
-    completed_on      TIMESTAMP      NOT NULL,
+    id           SERIAL PRIMARY KEY,
+    user_id      INTEGER        NOT NULL,
+    name         VARCHAR(255),
+    distance     DECIMAL(10, 2) NOT NULL,
+    started_at   TIMESTAMP      NOT NULL,
+    completed_on TIMESTAMP      NOT NULL,
     -- Foreign key
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE RESTRICT
 );
